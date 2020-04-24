84b614cc41ecc0008c375b2cec97d48ae15cafb0
wangcunlu <1137738840@qq.com>
2020-04-24 11:18:57 +0800

    123

---
+++ child0112/src/main/java/com/szdy/sasstest/SassTestApplication.java
@@ -0,0 +1,11 @@
+package com.szdy.sasstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SassTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(com.szdy.sasstest.SassTestApplication.class, args);
+    }
+}
---
+++ child123123/src/main/java/sasstest/SassTestApplication.java
@@ -0,0 +1,11 @@
+package sasstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SassTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SassTestApplication.class, args);
+    }
+}
